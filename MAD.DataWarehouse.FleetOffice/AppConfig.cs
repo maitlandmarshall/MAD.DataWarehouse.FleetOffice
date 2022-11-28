8daa8d392aebfcc5f1db49e81c9d3a5d14e23e2a
maitlandmarshall <maitlandmarshallapps@gmail.com>
2022-11-29 09:06:04 +1000

    Add project files.

---
+++ MAD.DataWarehouse.FleetOffice/AppConfig.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MAD.DataWarehouse.FleetOffice
+{
+    public class AppConfig
+    {
+        public string ConnectionString { get; set; }
+    }
+}
