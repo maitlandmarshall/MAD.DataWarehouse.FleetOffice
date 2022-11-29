98893c20b418dda61002ce50b29952c67b624c67
maitlandmarshall <maitlandmarshallapps@gmail.com>
2022-11-29 11:57:51 +1000

    Add OAuthB0ner and setup authentication handler

---
+++ MAD.DataWarehouse.FleetOffice/AuthenticationDelegatingHandler.cs
@@ -0,0 +1,26 @@
+﻿using OAuthB0ner.Storage;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MAD.DataWarehouse.FleetOffice
+{
+    internal class AuthenticationDelegatingHandler : DelegatingHandler
+    {
+        private readonly ITokenStorage tokenStorage;
+
+        public AuthenticationDelegatingHandler(ITokenStorage tokenStorage)
+        {
+            this.tokenStorage = tokenStorage;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var token = await this.tokenStorage.GetAccessToken();
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token.AccessToken);
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
--- MAD.DataWarehouse.FleetOffice/Startup.cs
+++ MAD.DataWarehouse.FleetOffice/Startup.cs
@@ -1,18 +1,47 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MIFCore.Common;
+using MIFCore.Hangfire.APIETL.Extract;
 using MIFCore.Settings;
+using OAuthB0ner;
+using System;
+using System.Threading.Tasks;
 
 namespace MAD.DataWarehouse.FleetOffice
 {
     internal class Startup
     {
+        private readonly IConfiguration configuration;
+
+        public Startup()
+        {
+            this.configuration = Globals.DefaultConfiguration;
+        }
+
         public void ConfigureServices(IServiceCollection serviceDescriptors)
         {
             serviceDescriptors.AddIntegrationSettings<AppConfig>();
+
+            serviceDescriptors.AddTransient<AuthenticationDelegatingHandler>();
+
+            serviceDescriptors
+                .AddHttpClient("fleet", cfg =>
+                {
+                    cfg.BaseAddress = new Uri("https://thefleetoffice.net.au:7080");
+                })
+                .AddHttpMessageHandler<AuthenticationDelegatingHandler>();
+
+            serviceDescriptors.AddEndpointsApiExplorer();
+
+            serviceDescriptors.AddOAuthB0ner(oauth =>
+            {
+                this.configuration.Bind("oauth", oauth);
+            });
         }
 
-        public void Configure()
+        public async Task Configure(IApiEndpointRegister apiEndpointRegister)
         {
-
+            await apiEndpointRegister.Register();
         }
     }
 }
